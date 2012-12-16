d1517d0b684149f89edd925c78238fd5a2674fba
Andrea Cecchi <andrea.cecchi85@gmail.com>
2012-12-16 03:35:09 +0100

    fixed checks for message factory loading

--- wildcard/foldercontents/resources/jquery-file-upload/js/locale.js
+++ wildcard/foldercontents/resources/jquery-file-upload/js/locale.js
@@ -16,22 +16,44 @@ var lang = jq('html').attr('lang');
  
 // get the translation tool catalog for the given language and domain
 jarn.i18n.loadCatalog('wildcard.foldercontents', lang);
+
 // let's create a message factory
-mf = jarn.i18n.MessageFactory('wildcard.foldercontents', lang);
-window.locale = {
-    "messagefactory": mf,
-    "fileupload": {
-        "errors": {
-            "maxFileSize": "File is too big",
-            "minFileSize": "File is too small",
-            "acceptFileTypes": "Filetype not allowed",
-            "maxNumberOfFiles": "Max number of files exceeded",
-            "uploadedBytes": "Uploaded bytes exceed file size",
-            "emptyResult": "Empty file upload result"
-        },
-        "error": "Error",
-        "start": "Start",
-        "cancel": "Cancel",
-        "destroy": "Delete"
-    }
-};
+    mf = jarn.i18n.MessageFactory('wildcard.foldercontents', lang);
+if (! jarn.i18n.catalogs['wildcard.foldercontents'][lang]) {
+    window.locale = {
+        "messagefactory": mf,
+        "fileupload": {
+            "errors": {
+                "maxFileSize": "File is too big",
+                "minFileSize": "File is too small",
+                "acceptFileTypes": "Filetype not allowed",
+                "maxNumberOfFiles": "Max number of files exceeded",
+                "uploadedBytes": "Uploaded bytes exceed file size",
+                "emptyResult": "Empty file upload result"
+            },
+            "error": "Error",
+            "start": "Start",
+            "cancel": "Cancel",
+            "destroy": "Delete"
+        }
+    };
+}
+else {
+    window.locale = {
+        "messagefactory": mf,
+        "fileupload": {
+            "errors": {
+                "maxFileSize": mf("File is too big"),
+                "minFileSize": mf("File is too small"),
+                "acceptFileTypes": mf("Filetype not allowed"),
+                "maxNumberOfFiles": mf("Max number of files exceeded"),
+                "uploadedBytes": mf("Uploaded bytes exceed file size"),
+                "emptyResult": mf("Empty file upload result")
+            },
+            "error": mf("Error"),
+            "start": mf("Start"),
+            "cancel": mf("Cancel"),
+            "destroy": mf("Delete")
+        }
+    };
+}
