545c85bc3480c54442bfa969d823a97c255a41e6
Johannes Raggam <raggam-nl@adm.at>
2014-07-03 16:32:26 +0200

    include js template from jstemplates module for a better overview

---
+++ wildcard/foldercontents/jstemplates.py
@@ -0,0 +1,61 @@
+NEW_FOLDER_CONTENTS_VIEW_JS_TEMPLATES = """
+<script id="template-upload" type="text/x-tmpl">
+{% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-upload">
+        <td class="preview"><span class=""></span></td>
+        <td class="name"><span>{%=file.name%}</span></td>
+        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+        {% if (file.error) { %}
+            <td class="error" colspan="2">
+                <span class="label label-important">
+                {%=locale.messagefactory(locale.fileupload.error)%}
+                </span> {%=locale.messagefactory(locale.fileupload.errors[file.error]) || file.error%}
+            </td>
+        {% } else if (o.files.valid && !i) { %}
+            <td>
+                <div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="bar" style="width:0%;"></div></div>
+            </td>
+            <td class="start">{% if (!o.options.autoUpload) { %}
+                <button class="btn btn-primary">
+                    <i class="icon-upload icon-white"></i>
+                    <span>{%=locale.messagefactory(locale.fileupload.start)%}</span>
+                </button>
+            {% } %}</td>
+        {% } else { %}
+            <td colspan="2"></td>
+        {% } %}
+        <td colspan="2" class="cancel">{% if (!i) { %}
+            <button class="btn btn-warning">
+                <i class="icon-ban-circle icon-white"></i>
+                <span>{%=locale.messagefactory(locale.fileupload.cancel)%}</span>
+            </button>
+        {% } %}</td>
+    </tr>
+{% } %}
+</script>
+<!-- The template to display files available for download -->
+<script id="template-download" type="text/x-tmpl">
+{% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-download">
+        {% if (file.error) { %}
+            <td></td>
+            <td class="name"><span>{%=file.name%}</span></td>
+            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+            <td class="error" colspan="2">
+                <span class="label label-important">
+                    {%=locale.messagefactory(locale.fileupload.error)%}
+                </span> {%=locale.messagefactory(locale.fileupload.errors[file.error]) || file.error%}</td>
+        {% } else { %}
+            <td class="preview">{% if (file.thumbnail_url) { %}
+                <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
+            {% } %}</td>
+            <td class="name">
+                <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
+            </td>
+            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+            <td colspan="2"></td>
+        {% } %}
+    </tr>
+{% } %}
+</script>
+"""
--- wildcard/foldercontents/views.py
+++ wildcard/foldercontents/views.py
@@ -1,5 +1,6 @@
 from .interfaces import IATCTFileFactory
 from .interfaces import IDXFileFactory
+from .jstemplates import NEW_FOLDER_CONTENTS_VIEW_JS_TEMPLATES
 from AccessControl import Unauthorized
 from Acquisition import aq_inner
 from Products.ATContentTypes.interfaces.topic import IATTopic
@@ -152,70 +153,10 @@ class NewFolderContentsView(FolderContentsView):
         return table.render()
 
     def jstemplates(self):
+        """Have to include js templates from view, because tal barfs when it's
+        in the page template
         """
-        have to put it here because tal barfs when it's in the template
-        """
-        return """
-<script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-upload">
-        <td class="preview"><span class=""></span></td>
-        <td class="name"><span>{%=file.name%}</span></td>
-        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
-        {% if (file.error) { %}
-            <td class="error" colspan="2">
-                <span class="label label-important">
-                {%=locale.messagefactory(locale.fileupload.error)%}
-                </span> {%=locale.messagefactory(locale.fileupload.errors[file.error]) || file.error%}
-            </td>
-        {% } else if (o.files.valid && !i) { %}
-            <td>
-                <div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="bar" style="width:0%;"></div></div>
-            </td>
-            <td class="start">{% if (!o.options.autoUpload) { %}
-                <button class="btn btn-primary">
-                    <i class="icon-upload icon-white"></i>
-                    <span>{%=locale.messagefactory(locale.fileupload.start)%}</span>
-                </button>
-            {% } %}</td>
-        {% } else { %}
-            <td colspan="2"></td>
-        {% } %}
-        <td colspan="2" class="cancel">{% if (!i) { %}
-            <button class="btn btn-warning">
-                <i class="icon-ban-circle icon-white"></i>
-                <span>{%=locale.messagefactory(locale.fileupload.cancel)%}</span>
-            </button>
-        {% } %}</td>
-    </tr>
-{% } %}
-</script>
-<!-- The template to display files available for download -->
-<script id="template-download" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
-    <tr class="template-download">
-        {% if (file.error) { %}
-            <td></td>
-            <td class="name"><span>{%=file.name%}</span></td>
-            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
-            <td class="error" colspan="2">
-                <span class="label label-important">
-                    {%=locale.messagefactory(locale.fileupload.error)%}
-                </span> {%=locale.messagefactory(locale.fileupload.errors[file.error]) || file.error%}</td>
-        {% } else { %}
-            <td class="preview">{% if (file.thumbnail_url) { %}
-                <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
-            {% } %}</td>
-            <td class="name">
-                <a href="{%=file.url%}" title="{%=file.name%}" data-gallery="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
-            </td>
-            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
-            <td colspan="2"></td>
-        {% } %}
-    </tr>
-{% } %}
-</script>
-"""
+        return NEW_FOLDER_CONTENTS_VIEW_JS_TEMPLATES
 
 
 def getOrdering(context):
